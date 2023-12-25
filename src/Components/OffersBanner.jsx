f7f9ab6ce4778ea6ebce2bc630e276b883b5aa44
cubejar <raj.avudaiappan@gmail.com>
2023-12-24 19:53:13 -0800

    Updated the Briyani logo text- OffersBanner.jsx

--- src/Components/OffersBanner.jsx
+++ src/Components/OffersBanner.jsx
@@ -10,7 +10,7 @@ function OffersBanner() {
     return (
         <div className=' d-flex justify-content-center align-items-center mt-5 flex-column'>
             <div className='w-75 '>
-                <h4 className='fw-bolder'>Best offers for you</h4>
+                <h4 className='fw-bolder'>The best offers for you from Cubejar!</h4>
                 <div id='banner-img' className='d-flex align-items-center ' style={{overflowY:"scroll",gap:"20px"}}>
                     <img width={'450px'} src={banner2} alt="Offer" />
                     <img width={'450px'} src={banner1} alt="Offer" />
@@ -21,11 +21,11 @@ function OffersBanner() {
 
             <div className='w-75 mt-5'>
                 
-                <h4 className='fw-bolder'>What's on your mind Yesterday!!!!?</h4>
+                <h4 className='fw-bolder'>What's on your mind Today!!!!?</h4>
                 <div className='d-flex align-items-center' style={{overflowY:"scroll",gap:"19px"}}>
-                    <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/rng/md/carousel/production/e20c602ba8ed5d8ec2204e7a7b19d9f6" alt="Puliyodharai!!" />
+                    <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/rng/md/carousel/production/e20c602ba8ed5d8ec2204e7a7b19d9f6" alt="Puliyodharai!!!" />
 
-                    <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/Biryani_2.png" alt="Biriyani" />
+                    <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/Biryani_2.png" alt="VegBiriyani" />
                     <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Burger.png" alt="Burger" />
                     <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029856/PC_Creative%20refresh/3D_bau/banners_new/Pizza.png" alt="Pizza" />
                     <img className='img-fluid' width={'140px'} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667626/PC_Creative%20refresh/South_Indian_4.png" alt="South Indian" />
